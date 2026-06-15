0da7b2aab81ccd01878722b08c1c7c98300b6a11
hc_aliyu_ecs <3227742107@qq.com>
2026-06-15 23:46:32 +0800

    2026-06-15 23:46:16 测试webhook是否起效

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
 # 2026-06-15 22:43:41 test push to github and make sure the function of webhook
+# 2026-06-15 23:43:13 几分钟前，我在上面的return语句中新增了两条，push app.py后，Jenkins并没有自动构建。所以我新增了这条句子
+# ---想再次验证一下webhook功能是否正常生效！
